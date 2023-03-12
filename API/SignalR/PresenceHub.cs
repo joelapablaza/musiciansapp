bdfe099a074c282d28611d8f27c896e264a4f62c
joelapablaza <joelapablaza@gmail.com>
2023-03-12 13:32:57 -0300

    more comments added

--- API/SignalR/PresenceHub.cs
+++ API/SignalR/PresenceHub.cs
@@ -42,3 +42,15 @@ namespace API.SignalR
         }
     }
 }
+
+/*
+Este código define una clase llamada "PresenceHub" que hereda de la clase "Hub" de SignalR, una biblioteca de tiempo real para .NET. La clase "PresenceHub" se utiliza para implementar la funcionalidad de seguimiento de presencia para usuarios conectados a una aplicación SignalR.
+
+El constructor de la clase "PresenceHub" toma un objeto "PresenceTracker" como argumento. El objeto "PresenceTracker" es una clase que se utiliza para realizar el seguimiento de los usuarios conectados y desconectados.
+
+El método "OnConnectedAsync()" se ejecuta cuando un nuevo usuario se conecta a la aplicación SignalR. En este método, se llama al método "UserConnected()" del objeto "PresenceTracker" para registrar al usuario como conectado y se obtiene un valor booleano que indica si el usuario estaba en línea antes de conectarse. Si el usuario estaba en línea antes de conectarse, se envía un mensaje a otros usuarios de la aplicación SignalR para notificarles que el usuario ha vuelto en línea. También se llama al método "GetOnlineUsers()" del objeto "PresenceTracker" para obtener una lista de los usuarios conectados y se envía a través de SignalR al usuario recién conectado utilizando el método "SendAsync()" de la clase "Clients".
+
+El método "OnDisconnectedAsync()" se ejecuta cuando un usuario se desconecta de la aplicación SignalR. En este método, se llama al método "UserDisconnected()" del objeto "PresenceTracker" para registrar al usuario como desconectado y se obtiene un valor booleano que indica si el usuario estaba en línea antes de desconectarse. Si el usuario estaba en línea antes de desconectarse, se envía un mensaje a otros usuarios de la aplicación SignalR para notificarles que el usuario ha abandonado la aplicación. Se llama al método "OnDisconnectedAsync()" de la clase "Hub" para permitir que SignalR realice las tareas de limpieza necesarias después de que se desconecte el usuario.
+
+En resumen, este código implementa una funcionalidad de seguimiento de presencia para usuarios conectados a una aplicación SignalR, utilizando un objeto "PresenceTracker" para realizar el seguimiento de los usuarios conectados y desconectados. Cuando un usuario se conecta o desconecta, se notifica a los otros usuarios de la aplicación SignalR para que puedan ver quiénes están conectados en tiempo real.
+*/
